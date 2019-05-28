02acf4dc77c3c506b4c99b7381f55d11d613c0f0
sepuckett86 <sepuckett86@gmail.com>
2019-05-28 13:32:55 -0700

    make App and append Header component

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import Component from './Component.js';
+import Header from './Header.js';
+
+class App extends Component {
+    render() {
+        const dom = this.renderDOM();
+        const header = new Header();
+        const headerDOM = header.render();
+        dom.prepend(headerDOM);
+        return dom;
+    }
+    renderTemplate() {
+        return /*html*/ `
+            <div>
+                <main>
+                    Hello
+                </main>
+            </div>
+        `;
+    }
+}
+
+export default App;
