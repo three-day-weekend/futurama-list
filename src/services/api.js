73de74df669f81f157b0a0a0e47a1bd95b44efbf
sepuckett86 <sepuckett86@gmail.com>
2019-05-30 09:19:50 -0700

    enable loading when search is null

--- src/services/api.js
+++ src/services/api.js
@@ -2,10 +2,14 @@ const URL = 'http://futuramaapi.herokuapp.com/api/quotes';
 
 const api = {
     getQuotes(search) {
-        const searchParams = new URLSearchParams();
-        searchParams.set('search', search);
-        const query = searchParams.toString();
-        return fetch(`${URL}?${query}`)
+        if(search) {
+            const searchParams = new URLSearchParams();
+            searchParams.set('search', search);
+            const query = searchParams.toString();
+            return fetch(`${URL}?${query}`)
+                .then(response => response.json());
+        }
+        return fetch(`${URL}`)
             .then(response => response.json());
     }
 };
